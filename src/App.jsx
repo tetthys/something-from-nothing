5dbd48720bcb660a0923700e17fc0ed294a58535
tetthys <jeongwoo0830z@gmail.com>
2024-03-14 22:31:52 +0900

    Add Modal component and update TodoInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import TodoInput from "./components/TodoInput";
 import WhatIsThis from "./components/WhatIsThis";
+import Modal from "./components/Modal";
 
 const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [todos, setTodos] = useState([{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]);
+
   const placeholders = [
     "$5 + 10 CHF = $10 if rate is 2:1",
     "$5 * 2 = $10",
@@ -11,15 +16,25 @@ const App = () => {
   ];
   return (
     <div className="w-full min-h-screen bg-cosmic-latte-500">
+      <Modal
+        open={isModalOpen}
+        onCloseHandler={() => {
+          setIsModalOpen(false);
+        }}
+      >
+        Have you just seen green bar?
+      </Modal>
       <div className="py-20  mx-auto w-full max-w-5xl">
         <WhatIsThis />
         <div className="mt-20 grid grid-cols-1 gap-y-10">
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((e, i) => (
+          {todos.map((e, i) => (
             <TodoInput
               key={i}
               name={"todo-input"}
               id={"todo-input"}
               placeholder={placeholders[i]}
+              onClickHandler={() => {}}
+              onChangeHandler={() => {}}
             />
           ))}
         </div>
